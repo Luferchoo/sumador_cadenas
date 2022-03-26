e00668d24ce6db98f62c47a448168422dc3e52c5
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-03-26 18:01:32 -0400

    TOMA EN CUENTA UNA CADENA CON DOS NÚMEROS SUMADOS POR UNA COMA

--- src/sumador.js
+++ src/sumador.js
@@ -1,6 +1,13 @@
 function sumar(cadena){
   var total = 0;
-  total = total + Number(cadena)
+  var array_num = cadena.split(",");
+  console.log(array_num.length);
+  if(array_num.length > 1){
+    total = total + Number(array_num[0]) + Number(array_num[1]);
+  }
+  else{
+    total = total + Number(cadena);
+  }
   return total
 }
 export default sumar;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -7,4 +7,7 @@ describe("Sumar Cadenas de Numeros", () => {
   it("TOMA EN CUENTA UNA CADENA CON UN SOLO NÚMERO", () => {
     expect(sumar("2")).toEqual(2);
   });
+  it("TOMA EN CUENTA UNA CADENA CON DOS NÚMEROS SUMADOS POR UNA COMA", () => {
+    expect(sumar("1,2")).toEqual(3);
+  });
 });
