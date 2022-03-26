903d406883ca62cb124012c7747684a9d69beb45
Luis Fernando Gil Gareca <luisfernandogilgareca@gmail.com>
2022-03-26 17:31:25 -0400

    TOMA EN CUENTA UNA CADENA CON UN SOLO NÚMERO

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,6 @@
 function sumar(cadena){
   var total = 0;
+  total = total + Number(cadena)
   return total
 }
 export default sumar;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,7 @@ describe("Sumar Cadenas de Numeros", () => {
   it("TOMA EN CUENTA UNA CADENA VACÍA", () => {
     expect(sumar("")).toEqual(0);
   });
+  it("TOMA EN CUENTA UNA CADENA CON UN SOLO NÚMERO", () => {
+    expect(sumar("2")).toEqual(2);
+  });
 });
